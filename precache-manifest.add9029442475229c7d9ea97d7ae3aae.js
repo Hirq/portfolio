c5057728039a17c8f2bb37b2b137f8b149391616
hirq <czarkooo@gmail.com>
2019-11-26 20:20:05 +0100

    Updates

---
+++ precache-manifest.add9029442475229c7d9ea97d7ae3aae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063f009ab1676ac430103d33e1765d76",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "347a9239b9b19805a978",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ef7750625535d47b3577",
+    "url": "/portfolio/static/js/2.9434d723.chunk.js"
+  },
+  {
+    "revision": "347a9239b9b19805a978",
+    "url": "/portfolio/static/js/main.e950a4c3.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  }
+]);
