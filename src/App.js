61332f2238af7481e5d0f7f581754d000252a799
hirq <czarkooo@gmail.com>
2019-12-02 23:02:25 +0100

    add image

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
@@ -13,23 +15,23 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaGgCircle } from "react-icons/fa";
+import { DiPython, DiDjango, DiHtml5, DiCss3, DiAngularSimple, DiSass, DiReact } from "react-icons/di";
 
 
 const useStyles = makeStyles(theme => ({
 
   topContainer:{
     minWidth:'100%',
-    backgroundColor: "#169565", 
-    minHeight: "50vh",
+    backgroundColor: "#2E576B",
+    minHeight: "400px",
     height:'100%', 
     border: '2px solid #fff', 
     borderRadius: '5px',
   },
 
   bottomContainer:{
-    backgroundColor: "#157450 ", 
-    minHeight: "100vh", 
+    backgroundColor: "", 
     height:"100%", 
     border: '3px solid #fff', 
     borderRadius: '5px',
@@ -38,7 +40,7 @@ const useStyles = makeStyles(theme => ({
   footer:{
     minWidth: "100%",
     padding: 0,
-    backgroundColor: "#169565", 
+    backgroundColor: "#2E576B", 
     minHeight: "15vh",
     height:'100%', 
     border: '2px solid #fff', 
@@ -48,50 +50,63 @@ const useStyles = makeStyles(theme => ({
   rootBottom: {
     width: "100%",
     padding: 15,
-    marginTop: '-10vh'
+    marginTop: '-100px'
   },
 
   rootTop: {
     width: "100%",
     padding: 50,
   },
-
-  list: {
-    border: '2px solid red',
-    borderRadius: '5px',
-    padding:35,
-    marginBottom:10,
-    textAlign: 'center',
-    backgroundColor: 'blue',
-  },
+///////////////
   icons:{
     textAlign:'right',
   },
 
   iconsElement:{
     color:'white',
+    marginLeft: '1vh',
+    "&:hover": {
+      padding: '1px',
+    },
   },
 
   name:{
     textAlign:'center',
     color: 'white',
     fontSize: '25px',
+    fontWeight: '700',
+    marginTop: '5vh',
   },
 
   introduce:{
     textAlign:'center',
-    color:'blue',
+    color:'black',
+    fontWeight: '450',
+    marginTop: '5vh',
+
   },
 
   buttons:{
     textAlign:'center',
-    color:'blue',
+    marginTop: '5vh',
+    marginBottom: '5vh',
   },
 
   buttonElement:{
-    marginLeft: '5px',
+    minWidth: '110px',
+    marginLeft: '10px',
+    marginRight: '10px',
+    marginBottom: '5vh',
     padding: 5,
-    backgroundColor: 'red',
+    color: 'white',
+    backgroundColor: '#20693C',
+    border: '1px solid #fff', 
+    borderRadius: '1vh',
+    "&:hover": {
+      backgroundColor: '#fff',
+      color: '#20693C',
+      borderRadius: '2vh',
+    },
   },
 
   footerButton:{
@@ -104,10 +119,122 @@ const useStyles = makeStyles(theme => ({
     marginTop: '3vh',
     color: 'white',
   },
+
+///////////LIST
+  list: {
+    border: '2px solid #0A1526',
+    borderRadius: '5px',
+    padding:35,
+    marginBottom:10,
+    textAlign: 'center',
+    backgroundColor: '#2C3C52',
+
+    "&:hover": {
+      backgroundColor: '#2C3C52',
+    },
+    "&:focus": {
+      backgroundColor: '#2C3C52',
+    },
+    "&:active": {
+      backgroundColor: '#2C3C52',
+    },
+  },
+
+  listContent:{
+    minWidth: '70%',
+    textAlign: 'left',
+  },
+
+  listPhoto:{
+    minWidth: '30%',
+    backgroundImage: `url(${'https://i.vimeocdn.com/portrait/9798022_300x300'})`,
+    minHeight: '200px',
+    maxHeight: '100px',
+  },
+  listHeader:{
+  },
+
+  listName:{
+    fontWeight: '600',
+    marginRight: '2vh',
+    fontSize: '25px',
+    display: 'inline-block',
+  },
+  listIcons:{
+    display: 'inline-block',
+  },
+  listIconsElement:{
+    color:'white',
+  },
+  listIconsElementDjango:{
+    color:'white',
+    fontSize: '30px',
+    marginBottom: '-7px',
+  },
+
+  listDescribe:{
+    marginTop:'3vh',
+    fontWeight: '300',
+  },
+  listButton:{
+    marginTop:'5vh',
+  },
+  listButtonElement:{
+    minWidth: '90px',
+    color: 'white',
+    backgroundColor: '#20693C',
+    border: '1px solid #fff', 
+    borderRadius: '1vh',
+    marginRight: '20px',
+    "&:hover": {
+      backgroundColor: '#fff',
+      color: '#20693C',
+    },
+  },
+  listButtonElement2:{
+    minWidth: '90px',
+    color: 'white',
+    backgroundColor: '#20693C',
+    border: '1px solid #fff', 
+    borderRadius: '1vh',
+    "&:hover": {
+      backgroundColor: '#fff',
+      color: '#20693C',
+    },
+  },
+    listButtonIconsElement:{
+      marginLeft: '10px',
+  },
+
+  //footer
+
+  buttonElementFooter:{
+    minWidth: '110px',
+    padding: 5,
+    color: 'white',
+    backgroundColor: '#20693C',
+    border: '1px solid #fff', 
+    borderRadius: '1vh',
+    "&:hover": {
+      backgroundColor: '#fff',
+      color: '#20693C',
+      borderRadius: '2vh',
+    },
+  },
 }));
 
 function ListItemLink(props) {
   return <ListItem button component="a" {...props} />;
+}
+
+function Redux() {
+  // Import result is the URL of your image
+  return <img src={'/redux.png'} alt="Redux" style={{tintColor:'red', maxWidth:'15px'}}/>;
+}
+
+function LoquestZDJ() {
+  // Import result is the URL of your image
+  return <img src={'https://lh3.googleusercontent.com/RivvntB-z-qe59FF0j9bxy6tBXZCnksHIz_EvXvaSKa039MC9IwZLTORIirdJcZZcE8'} alt="Redux" style={{tintColor:'red', }}/>;
 }
 
 export default function SimpleList() {
@@ -117,57 +244,202 @@ export default function SimpleList() {
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="sm" className={classes.topContainer}>
-        <Container maxWidth="sm" className={classes.rootTop}>
+        <Container  className={classes.rootTop}>
 
           <Typography component="div" className={classes.icons}>
-            <FaGithub className={classes.iconsElement}/>
+            <a href="https://github.com/Hirq">
+              <FaGithub className={classes.iconsElement}/>
+            </a>
+            <a href="https://pl.linkedin.com/in/cezary-zabrocki/">
             <FaLinkedin className={classes.iconsElement}/>
+            </a>
           </Typography>
           <Typography component="div" className={classes.name}>
             Hello My name is Cezary and this is my portfolio!
           </Typography>
           <Typography component="h2" className={classes.introduce}>
-            Junior developer with basics react,angular and python.
+            Junior developer with basics react, angular and python.
           </Typography>
           <Typography component="h2" className={classes.buttons}>
             <Button size="small" className={classes.buttonElement}>Portfolio</Button>
+            <a href="https://drive.google.com/file/d/1DoQU7gAMd7n7Tp5tQcZrWERKTVwlxTA-/view?usp=sharing">
             <Button size="small" className={classes.buttonElement}>CV</Button>
+            </a>
           </Typography>
         </Container>
       </Container>
-
-
-      <Container maxWidth="sm" className={classes.bottomContainer} >
+      <Container className={classes.bottomContainer} >
         <Typography component="div">
           <div className={classes.rootBottom}>
             <List component="nav" aria-label="main mailbox folders">
+
               <ListItem button className={classes.list}>
-                <ListItemIcon>
-                  <InboxIcon />
-                </ListItemIcon>
-                <ListItemText primary="Inbox" />
+                <div className={classes.listContent}>
+                  <div className={classes.listHeader}>
+                    <div className={classes.listName}>
+                    Loquest
+                    </div>
+                    <div className={classes.listIcons}>
+                     <DiPython className={classes.listIconsElement}/>
+                     <DiDjango className={classes.listIconsElementDjango}/>
+                   
+                     <DiHtml5 className={classes.listIconsElement}/>
+                     <DiCss3 className={classes.listIconsElement}/>
+                    </div>   
+                  </div>
+                  <div className={classes.listDescribe}>
+                    Pierwsza moja aplikacja fullstack w całości zbudowana na bibliotece Django.
+                  </div>
+                  <div className={classes.listButton}>
+                    <a href="https://github.com/Hirq/Loquest">
+                      <Button size="small" className={classes.listButtonElement}>
+                      GitHub
+                      <FaGithub className={classes.listButtonIconsElement}/>
+                      </Button>
+                    </a>
+                    <a href="http://hirq.ct8.pl/">
+                    <Button size="small" className={classes.listButtonElement2}>
+                    Online
+                    <FaGgCircle className={classes.listButtonIconsElement}/>
+                    </Button>
+                    </a>
+                  </div>
+                </div>
+                <div className={classes.listPhoto}>
+                  ZDJ
+                </div>
               </ListItem>
+
               <ListItem button className={classes.list}>
-                <ListItemIcon>
-                  <DraftsIcon />
-                </ListItemIcon>
-                <ListItemText primary="Drafts" />
+                <div className={classes.listContent}>
+                  <div className={classes.listHeader}>
+                    <div className={classes.listName}>
+                      Majshop
+                    </div>
+                    <div className={classes.listIcons}>
+                     <DiPython className={classes.listIconsElement}/>
+                     <DiDjango className={classes.listIconsElementDjango}/>
+                   
+                     <DiHtml5 className={classes.listIconsElement}/>
+                     <DiCss3 className={classes.listIconsElement}/>
+                    </div>   
+                  </div>
+                  <div className={classes.listDescribe}>
+                    Sklep internetowy z gitarami oraz efektami gitarowymi.
+                  </div>
+                  <div className={classes.listButton}>
+                    <a href="https://github.com/Hirq/Shop/tree/onlyDjango">
+                      <Button size="small" className={classes.listButtonElement}>
+                      GitHub
+                      <FaGithub className={classes.listButtonIconsElement}/>
+                      </Button>
+                    </a>
+                    <a href="http://hirq-shop.ct8.pl/">
+                    <Button size="small" className={classes.listButtonElement2}>
+                    Online
+                    <FaGgCircle className={classes.listButtonIconsElement}/>
+                    </Button>
+                    </a>
+                  </div>
+                </div>
+                <div className={classes.listPhoto}>
+                  zdj
+                </div>
               </ListItem>
+
+              <ListItem button className={classes.list}>
+                <div className={classes.listContent}>
+                  <div className={classes.listHeader}>
+                    <div className={classes.listName}>
+                      FindDev
+                    </div>
+                    <div className={classes.listIcons}>
+                     <DiAngularSimple className={classes.listIconsElement}/>
+                     <DiHtml5 className={classes.listIconsElement}/>
+                     <DiSass className={classes.listIconsElement}/>
+                    </div>   
+                  </div>
+                  <div className={classes.listDescribe}>
+                  W trakcie produkcji.
+                  <br/>
+                  Serwis gdzie programista się ogłsza, że szuka pracy.<br/>
+                  </div>
+                  <div className={classes.listButton}>
+                    <a href="https://github.com/Hirq/FindDev">
+                      <Button size="small" className={classes.listButtonElement}>
+                      GitHub
+                      <FaGithub className={classes.listButtonIconsElement}/>
+                      </Button>
+                    </a>
+                    <a href="XXX">
+                    <Button size="small" className={classes.listButtonElement2}>
+                      view
+                    <FaGgCircle className={classes.listButtonIconsElement}/>
+                    </Button>
+                    </a>
+                  </div>
+                </div>
+                <div className={classes.listPhoto}>
+                  ZDJ
+                </div>
+              </ListItem>
+
+              <ListItem button className={classes.list}>
+                <div className={classes.listContent}>
+                  <div className={classes.listHeader}>
+                    <div className={classes.listName}>
+                      Woradd
+                    </div>
+                    <div className={classes.listIcons}>
+                     <DiReact className={classes.listIconsElement}/>
+                      <Redux/>
+                     <DiHtml5 className={classes.listIconsElement}/>
+                     <DiSass className={classes.listIconsElement}/>
+                    </div>   
+                  </div>
+                  <div className={classes.listDescribe}>
+                    W trakcie produkcji. 
+                      <br/>
+                    Apliakacja do nauki jezyka angielskiego. <br/>
+                  </div>
+                  <div className={classes.listButton}>
+                    <a href="https://github.com/Hirq/Woradd">
+                      <Button size="small" className={classes.listButtonElement}>
+                      GitHub
+                      <FaGithub className={classes.listButtonIconsElement}/>
+                      </Button>
+                    </a>
+                    <a href="XXX">
+                    <Button size="small" className={classes.listButtonElement2}>
+                      view
+                    <FaGgCircle className={classes.listButtonIconsElement}/>
+                    </Button>
+                    </a>
+                  </div>
+                </div>
+                <div className={classes.listPhoto}>
+                  ZDJ
+                </div>
+              </ListItem>
+
             </List>
-          </div>
-          <div className={classes.kontakt}>
-            Kontakt
           </div>
         </Typography>
       </Container>
 
       <Container maxWidth="sm" className={classes.footer}>
         <Typography component="div" className={classes.footerButton}>
-          <Button size="small">Button</Button>
+          <Button size="small" className={classes.buttonElementFooter}>
+            Contact
+          </Button>
         </Typography>
         <Typography component="div" className={classes.footerIcons}>
-          <FaGithub />
-          <FaLinkedin />
+          <a href="https://github.com/Hirq/">
+            <FaGithub className={classes.iconsElement}/>
+          </a>
+          <a href="https://pl.linkedin.com/in/cezary-zabrocki/">
+            <FaLinkedin className={classes.iconsElement}/>
+          </a>
         </Typography>
       </Container>
     </React.Fragment>
