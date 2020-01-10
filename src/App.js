c0b6431fd7c87377a437a35ef2a55985b01d7e59
hirq <czarkooo@gmail.com>
2020-01-10 19:41:01 +0100

    add images

--- src/App.js
+++ src/App.js
@@ -631,13 +631,42 @@ export default function SimpleList() {
 
                 </div>
                 <div className={classes.listPhoto}>
-                  ZDJ
+                  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img class="d-block w-100" src="https://miro.medium.com/max/5920/1*AiVwCy6dXORN0j-0i9Jw5w.png" alt="First slide"></img>
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100" src="https://miro.medium.com/max/5920/1*AiVwCy6dXORN0j-0i9Jw5w.png" alt="Second slide"></img>
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100" src="https://miro.medium.com/max/5920/1*AiVwCy6dXORN0j-0i9Jw5w.png" alt="Third slide"></img>
+                      </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
                 </div>
               </ListItem>
             </List>
           </div>
         </Typography>
       </Container>
+
+
+
+
       <Container maxWidth="sm" className={classes.footer}>
         <Typography component="div" className={classes.footerButton}>
           <Button size="small" className={classes.buttonElementFooter} onClick={handleOpen}>
