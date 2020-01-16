8b084b4a4bf402f51efddd251d9f259add631710
hirq <czarkooo@gmail.com>
2020-01-16 20:51:23 +0100

    Updates

---
+++ precache-manifest.a512d6b9a11a4ac59a96493651208f32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc7166cbcb612b1b94726f02077be59",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "f239ef4d70c85fbde131",
+    "url": "/portfolio2/static/css/main.f65179ab.chunk.css"
+  },
+  {
+    "revision": "cb8a80d63657d6ba9518",
+    "url": "/portfolio2/static/js/2.d3ff59e3.chunk.js"
+  },
+  {
+    "revision": "f239ef4d70c85fbde131",
+    "url": "/portfolio2/static/js/main.af37ceed.chunk.js"
+  },
+  {
+    "revision": "ed53229d306b93a3a00d",
+    "url": "/portfolio2/static/js/runtime-main.76193d97.js"
+  }
+]);
